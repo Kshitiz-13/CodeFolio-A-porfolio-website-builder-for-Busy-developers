3abc6a19374ef0726c152c6c8ece5c96bd3d4074
Kshitiz <kshitizmunjal2@gmail.com>
2022-06-09 15:18:00 +0530

    Version 1 completed

---
+++ projects/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+import uuid
+from django.db.models.deletion import CASCADE
+from users.models import Profile
+# Create your models here.
+
+
+class Project(models.Model):
+    owner = models.ForeignKey(
+        Profile, null=True, blank=True, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    featured_image = models.ImageField(
+        null=True, blank=False, default="project-default.png")
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
+
+   
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.featured_image.url
+        except:
+            url = " "
+        return url
+
